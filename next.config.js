09e72485312343c3db28f743c2374ff0fa9c1e84
bchiang7 <brittany.chiang@gmail.com>
2019-06-17 09:58:01 -0400

    Maybe fix now things

--- next.config.js
+++ next.config.js
@@ -1,12 +1,3 @@
 module.exports = {
-  webpack: config => {
-    // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: 'empty',
-    };
-
-    return config;
-  },
-
   target: 'serverless',
 };
